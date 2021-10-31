27768b458629fb15fd9a862d54e37b9a3115111a
Grey Thomas <83403671+greysk@users.noreply.github.com>
2021-10-31 05:50:12 -0400

    Style to match PEP 8. Switch to f-string.

--- rock-paper-scissor-0.py
+++ rock-paper-scissor-0.py
@@ -3,30 +3,39 @@ class Participant:
         self.name = name
         self.points = 0
         self.choice = ""
+
     def choose(self):
-        self.choice = input("{name}, select rock, paper or scissor: ".format(name=self.name))
-        print("{name} selects {choice}".format(name=self.name, choice=self.choice))
+        self.choice = input(f"{self.name}, select rock, paper or scissor: ")
+        print(f"{self.name} selects {self.choice}")
+
 
 class GameRound:
     def __init__(self, p1, p2):
         p1.choose()
         p2.choose()
+
     def compareChoices(self):
         print("implement")
+
     def awardPoints(self):
         print("implement")
+
 
 class Game:
     def __init__(self):
         self.endGame = False
         self.participant = Participant("Spock")
         self.secondParticipant = Participant("Kirk")
+
     def start(self):
         game_round = GameRound(self.participant, self.secondParticipant)
+
     def checkEndCondition(self):
         print("implement")
+
     def determineWinner(self):
         print("implement")
+
 
 game = Game()
 game.start()
