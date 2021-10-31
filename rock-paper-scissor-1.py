c6560821b733cbe4efb8f37be50eb89ed6a507a7
Grey Thomas <83403671+greysk@users.noreply.github.com>
2021-10-31 05:52:21 -0400

    Style to match PEP 8. Switch to f-strings

--- rock-paper-scissor-1.py
+++ rock-paper-scissor-1.py
@@ -3,9 +3,11 @@ class Participant:
         self.name = name
         self.points = 0
         self.choice = ""
+
     def choose(self):
-        self.choice = input("{name}, select rock, paper or scissor: ".format(name=self.name))
-        print("{name} selects {choice}".format(name=self.name, choice=self.choice))
+        self.choice = input(f"{self.name}, select rock, paper or scissor: ")
+        print(f"{self.name} selects {self.choice}")
+
     def toNumericalChoice(self):
         switcher = {
             "rock": 0,
@@ -13,6 +15,7 @@ class Participant:
             "scissor": 2
         }
         return switcher[self.choice]
+
 
 class GameRound:
     def __init__(self, p1, p2):
@@ -24,11 +27,14 @@ class GameRound:
         p1.choose()
         p2.choose()
         result = self.compareChoices(p1, p2)
-        print("Round resulted in a {result}".format(result=self.getResultAsString(result)))
+        print(f"Round resulted in a {self.getResultAsString(result)}")
+
     def compareChoices(self, p1, p2):
         return self.rules[p1.toNumericalChoice()][p2.toNumericalChoice()]
+
     def awardPoints(self):
         print("implement")
+
     def getResultAsString(self, result):
         res = {
             0: "draw",
@@ -37,17 +43,22 @@ class GameRound:
         }
         return res[result]
 
+
 class Game:
     def __init__(self):
         self.endGame = False
         self.participant = Participant("Spock")
         self.secondParticipant = Participant("Kirk")
+
     def start(self):
         game_round = GameRound(self.participant, self.secondParticipant)
+
     def checkEndCondition(self):
         print("implement")
+
     def determineWinner(self):
         print("implement")
+
 
 game = Game()
 game.start()
